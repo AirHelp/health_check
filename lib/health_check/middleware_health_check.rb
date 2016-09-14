7d4f2b9382d2d024375e2699ac1ed4a1a7a5f98f
Filip <filip.haftek@airhelp.com>
2016-09-14 09:28:46 +0200

    Fixed middleware

--- lib/health_check/middleware_health_check.rb
+++ lib/health_check/middleware_health_check.rb
@@ -6,21 +6,21 @@ module HealthCheck
     end
 
     def call(env)
-      uri = env['PATH_INFO'.freeze]
+      uri = env['PATH_INFO']
       if uri.include? HealthCheck.uri
-        response_type = uri.sub! '/' + HealthCheck.uri + '.' , ''
-        response_method = 'response_' + response_type.to_s
-        checks = 'standard'
+        response_type = uri[/\.(json|xml)/,1]
+        response_method = 'response_' + (response_type || 'plain').to_s
+        checks = env['QUERY_STRING'][/checks=([a-z0-9\-]*)/,1] || 'standard'
         begin
           errors = HealthCheck::Utils.process_checks(checks)
-        rescue Exception => e
+        rescue => e
           errors = e.message.blank? ? e.class.to_s : e.message.to_s
         end
         if errors.blank?
-          return send(response_method, 200, HealthCheck.success, true)
+          send(response_method, 200, HealthCheck.success, true)
         else
           msg = "health_check failed: #{errors}"
-          return send(response_method, 500, msg, false)
+          send(response_method, 500, msg, false)
         end
       else
         @app.call(env)
@@ -29,14 +29,16 @@ module HealthCheck
 
     def response_json code, msg, healthy
       obj = { healthy: healthy, message: msg }
-      return [ code, { 'Content-Type' => 'application/json' }, [obj.to_json] ]
+      [ code, { 'Content-Type' => 'application/json' }, [obj.to_json] ]
     end
+
     def response_xml code, msg, healthy
       obj = { healthy: healthy, message: msg }
-      return [ code, { 'Content-Type' => 'text/xml' }, [obj.to_xml] ]
+      [ code, { 'Content-Type' => 'text/xml' }, [obj.to_xml] ]
     end
-    def response_ code, msg, healthy
-      return [ code, { 'Content-Type' => 'text/plain' }, [msg] ]
+
+    def response_plain code, msg, healthy
+      [ code, { 'Content-Type' => 'text/plain' }, [msg] ]
     end
   end
 end
